209142e7a5df94af379db1aa9c96e4582d26f93c
yulia_04 <yulichka.dudij@gmail.com>
2021-07-19 20:01:48 +0300

    Developed component for searching patient

---
+++ angularPackage/src/app/component/patient-search/patient-search.component.ts
@@ -0,0 +1,50 @@
+import {Component, OnInit} from '@angular/core';
+import {Patient} from "../../model/patient";
+import {Observable, Subject} from "rxjs";
+import {PatientService} from "../../service/patient-service.service";
+import {debounceTime, distinctUntilChanged, switchMap} from "rxjs/operators";
+
+@Component({
+  selector: 'app-patient-search',
+  templateUrl: './patient-search.component.html',
+  styleUrls: ['./patient-search.component.css']
+})
+export class PatientSearchComponent implements OnInit {
+
+  patients$: Observable<Patient[]>;
+  private searchTerms = new Subject<string>();
+  private isVisible = false;
+
+  constructor(private patientService: PatientService) {
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.patients$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.patientService.searchPatient(term)),
+    );
+  }
+
+  show(): void {
+    this.isVisible = true;
+  }
+
+  async hide() {
+    await this.delay(200);
+    this.isVisible = false;
+  }
+
+  async delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+}
