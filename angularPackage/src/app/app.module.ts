5c693b5ba71af6d3d7d8c453de23a85e28509ed8
yulia_04 <yulichka.dudij@gmail.com>
2021-07-20 17:30:28 +0300

    Common setting

--- angularPackage/src/app/app.module.ts
+++ angularPackage/src/app/app.module.ts
@@ -5,31 +5,49 @@ import { NgModule } from '@angular/core';
 // import * as dayjs from 'dayjs'
 
 
+
 import { AppComponent } from './app.component';
 // import {RouterModule} from "@angular/router";
 import { PatientListComponent } from './component/patient-list/patient-list.component';
 import {AppRoutingModule} from "./module/app-routing/app-routing.module";
 import {HttpClientModule} from "@angular/common/http";
 import {PatientService} from "./service/patient-service.service";
+import {CommentService} from "./service/comment-service.service";
 import { PatientViewComponent } from './component/patient-view/patient-view.component';
+import { CommentListComponent } from './component/comment-list/comment-list.component';
+import { PatientSearchComponent } from './component/patient-search/patient-search.component';
+import { CommentHandlerComponent } from './component/comment-handler/comment-handler.component';
+import { DoctorListComponent } from './component/doctor-list/doctor-list.component';
+import {DoctorService} from "./service/doctor-service.service";
+import {FormsModule} from "@angular/forms";
+// import {MatFormFieldModule} from "@angular/material";
 
+declare global {
+  type unknown = any
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     PatientListComponent,
     PatientViewComponent,
+    CommentListComponent,
+    PatientSearchComponent,
+    CommentHandlerComponent,
+    DoctorListComponent,
   ],
   imports: [
     BrowserModule,
     // RouterModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    // MatFormFieldModule,
     // AngularDayjsModule
     // MatPaginatorModule
 
   ],
-  providers: [PatientService],
+  providers: [PatientService, CommentService, DoctorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angularPackage/src/app/module/app-routing/app-routing.module.ts
+++ angularPackage/src/app/module/app-routing/app-routing.module.ts
@@ -3,13 +3,21 @@ import {CommonModule} from '@angular/common';
 import {Routes, RouterModule} from '@angular/router';
 import {PatientListComponent} from "../../component/patient-list/patient-list.component";
 import {PatientViewComponent} from "../../component/patient-view/patient-view.component";
+import {CommentListComponent} from "../../component/comment-list/comment-list.component";
+import {DoctorListComponent} from "../../component/doctor-list/doctor-list.component";
 
 const routes: Routes = [
+  {path: '', redirectTo: '/patients', pathMatch: 'full'},
   {
     path: 'patients', component: PatientListComponent,
-    children: [{path: 'detail/:id', component: PatientViewComponent}]
-  }
-  // { path: 'adduser', component: UserFormComponent }
+    children: [{path: 'detail/:id', component: PatientViewComponent},
+      {path: 'doctors', component: DoctorListComponent }
+    ]
+      // children:[{path: 'comments', component: CommentListComponent}]}]
+    // children: [{path: 'detail/:id', component: PatientViewComponent}]
+  },
+  // {path: 'patients/detail/:id/comments', component: CommentListComponent}
+  {path: 'doctors', component: DoctorListComponent }
 ];
 
 @NgModule({
