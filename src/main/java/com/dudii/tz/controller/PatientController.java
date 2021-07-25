03a5e0badba5a4b763fabc1b778483b7e08ede79
yulia_04 <yulichka.dudij@gmail.com>
2021-07-25 21:00:43 +0300

    updates crossOrigin in controllers

--- src/main/java/com/dudii/tz/controller/PatientController.java
+++ src/main/java/com/dudii/tz/controller/PatientController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PatientController {
 
     private final PatientService patientService;
